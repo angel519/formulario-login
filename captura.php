f5f791ece859cd4b6a476265eda5f0ee13f34d74
angel <niqelnegro@gmail.com>
2020-10-10 15:13:14 -0500

    formulario con php

---
+++ captura.php
@@ -0,0 +1,22 @@
+<?php 
+
+echo "formulario de envio de datos con POST/GET";
+
+
+$username = $_REQUEST['username'];
+$password = $_REQUEST['password'];
+
+echo "<br/>";
+echo "<br/>";
+echo $username;
+echo "<br/>";
+echo "$password";
+
+
+
+
+
+
+
+
+ ?>
